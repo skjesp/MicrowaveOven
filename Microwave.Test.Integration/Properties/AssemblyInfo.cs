672dd07d135596a3116275773461e78743575df3
rasmus godske <rasmusgodske@gmail.com>
2019-04-23 13:23:06 +0200

    Added integration test project

---
+++ Microwave.Test.Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "Microwave.Test.Integration" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Microwave.Test.Integration" )]
+[assembly: AssemblyCopyright( "Copyright ©  2019" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "f75d0ceb-d85e-49e6-ae3d-07edc9e70ae9" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
