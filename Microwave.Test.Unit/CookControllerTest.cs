b44316ec2f860ee5e78b93370c3b021c576c0d49
rasmus godske <rasmusgodske@gmail.com>
2019-04-25 15:45:17 +0200

    Fixed unit test: Coooking_TimerTick_DisplayCalled(). Classes uses different units

--- Microwave.Test.Unit/CookControllerTest.cs
+++ Microwave.Test.Unit/CookControllerTest.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using MicrowaveOvenClasses.Controllers;
+﻿using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
+using System;
 
 namespace Microwave.Test.Unit
 {
@@ -24,42 +24,44 @@ namespace Microwave.Test.Unit
             display = Substitute.For<IDisplay>();
             powerTube = Substitute.For<IPowerTube>();
 
-            uut = new CookController(timer, display, powerTube, ui);
+            uut = new CookController( timer, display, powerTube, ui );
         }
 
         [Test]
         public void StartCooking_ValidParameters_TimerStarted()
         {
-            uut.StartCooking(50, 60);
+            uut.StartCooking( 50, 60 );
 
-            timer.Received().Start(60);
+            timer.Received().Start( 60 );
         }
 
         [Test]
         public void StartCooking_ValidParameters_PowerTubeStarted()
         {
-            uut.StartCooking(50, 60);
+            uut.StartCooking( 50, 60 );
 
-            powerTube.Received().TurnOn(50);
+            powerTube.Received().TurnOn( 50 );
         }
 
         [Test]
         public void Cooking_TimerTick_DisplayCalled()
         {
-            uut.StartCooking(50, 60);
+            uut.StartCooking( 50, 60 );
 
-            timer.TimeRemaining.Returns(115);
-            timer.TimerTick += Raise.EventWith(this, EventArgs.Empty);
+            // FOUND ERROR:
+            // CookingController expects timer to use seconds instead of milliseconds
+            timer.TimeRemaining.Returns( 115 * 1000 );
+            timer.TimerTick += Raise.EventWith( this, EventArgs.Empty );
 
-            display.Received().ShowTime(1, 55);
+            display.Received().ShowTime( 1, 55 );
         }
 
         [Test]
         public void Cooking_TimerExpired_PowerTubeOff()
         {
-            uut.StartCooking(50, 60);
+            uut.StartCooking( 50, 60 );
 
-            timer.Expired += Raise.EventWith(this, EventArgs.Empty);
+            timer.Expired += Raise.EventWith( this, EventArgs.Empty );
 
             powerTube.Received().TurnOff();
         }
@@ -67,9 +69,9 @@ namespace Microwave.Test.Unit
         [Test]
         public void Cooking_TimerExpired_UICalled()
         {
-            uut.StartCooking(50, 60);
+            uut.StartCooking( 50, 60 );
 
-            timer.Expired += Raise.EventWith(this, EventArgs.Empty);
+            timer.Expired += Raise.EventWith( this, EventArgs.Empty );
 
             ui.Received().CookingIsDone();
         }
@@ -77,7 +79,7 @@ namespace Microwave.Test.Unit
         [Test]
         public void Cooking_Stop_PowerTubeOff()
         {
-            uut.StartCooking(50, 60);
+            uut.StartCooking( 50, 60 );
             uut.Stop();
 
             powerTube.Received().TurnOff();
