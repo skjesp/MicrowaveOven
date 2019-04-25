45edcedab1be9b02a165ceafa8468621cbab479a
Johannes Sebastian K C Jespersen <201606235@post.au.dk>
2019-04-25 14:25:54 +0200

    Changed IT0 to raising events

--- Microwave.Test.Integration/IT0_UserInterface.cs
+++ Microwave.Test.Integration/IT0_UserInterface.cs
@@ -4,6 +4,7 @@ using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
+using System;
 using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace Microwave.Test.Integration
@@ -16,7 +17,7 @@ namespace Microwave.Test.Integration
         private IButton _powerbutton = new Button();
         private IButton _timeButton = new Button();
         private IButton _startCancelButton = new Button();
-        private IDoor _door = new Door();
+        private IDoor _door;
         private ILight _light;
         private IDisplay _display;
         private IUserInterface _userInterface;
@@ -27,6 +28,7 @@ namespace Microwave.Test.Integration
             _cookController = Substitute.For<ICookController>();
             _output = Substitute.For<IOutput>();
             _display = Substitute.For<IDisplay>();
+            _door = Substitute.For<IDoor>();
             _light = new Light(_output);
             _userInterface = new UserInterface(_powerbutton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
         }
@@ -34,15 +36,15 @@ namespace Microwave.Test.Integration
         [Test]
         public void TurnOn_Ready_TurnOnHasBeenCalledOnce()
         {
-            _door.Open();
+            _door.Opened += Raise.EventWith(this, EventArgs.Empty);
             _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("on")));
         }
 
         [Test]
         public void TurnOff_Ready_TurnOffHasBeenCalledOnce()
         {
-            _door.Open();
-            _door.Close();
+            _door.Opened += Raise.EventWith(this, EventArgs.Empty);
+            _door.Closed += Raise.EventWith(this, EventArgs.Empty);
             _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("off")));
         }
     }
