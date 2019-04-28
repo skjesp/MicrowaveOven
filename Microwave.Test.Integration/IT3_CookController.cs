e7e7120a1be0ec281c3c975a9ea3dc7ea561770a
Mads Bjerg <au566699@uni.au.dk>
2019-04-28 13:52:20 +0200

    Setup for IT3 complete

--- Microwave.Test.Integration/IT3_CookController.cs
+++ Microwave.Test.Integration/IT3_CookController.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+
 
 namespace Microwave.Test.Integration
 {
     class IT3_CookController
     {
+        private CookController _uut;
+        private IUserInterface _userInterface;
+        private ITimer _timer;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
+        private IOutput _output;
+
+        [SetUp]
+        void Init()
+        {
+            _userInterface = Substitute.For<IUserInterface>();                      // Substituting Interface
+            _timer = Substitute.For<ITimer>();                                      // Substituting Timer
+
+            _output = Substitute.For<IOutput>();                                    // Substituting Output
+            _display = new Display(_output);                                        // Using real Display
+
+            _powerTube = new PowerTube(_output);                                    // Using real PowerTube
+            _uut = new CookController(_timer,_display,_powerTube,_userInterface);   // UUT Constructor.
+        }
     }
 }
